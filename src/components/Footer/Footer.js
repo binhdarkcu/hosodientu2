9781a0dea9d32d548957bb591c6e1b35a1c477a1
Thanh Che <cqthanh.zx@gmail.com>
2019-04-21 17:31:39 +0700

    added components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+
+  componentDidMount(){
+    console.log('This is console log from Footer.js');
+  }
+
+  render(){
+    return(
+      <div>Footer</div>
+    );
+  }
+}
+export default Footer;
