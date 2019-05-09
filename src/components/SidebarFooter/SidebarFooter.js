273fff425e6cc72dadadcf404f3666ea45371556
Thanh Che <cqthanh@tma.com.vn>
2019-05-09 15:20:37 +0700

    [Update] Fix links issues

--- src/components/SidebarFooter/SidebarFooter.js
+++ src/components/SidebarFooter/SidebarFooter.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SidebarFooter = () =>{
-  return(
-    <div className="sidebar-footer hidden-small">
-      <a data-toggle="tooltip" data-placement="top" title="Settings">
-        <span className="glyphicon glyphicon-cog" aria-hidden="true"></span>
-      </a>
-      <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-        <span className="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-      </a>
-      <a data-toggle="tooltip" data-placement="top" title="Lock">
-        <span className="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-      </a>
-      <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
-        <span className="glyphicon glyphicon-off" aria-hidden="true"></span>
-      </a>
-    </div>
-  );
+class SidebarFooter extends Component{
+
+  handleClick = (e) => {
+    e.preventDefault();
+  }
+
+  render(){
+    return(
+      <div className="sidebar-footer hidden-small">
+        <a href="index.html" onClick={this.handleClick} data-toggle="tooltip" data-placement="top" title="Settings">
+          <span className="glyphicon glyphicon-cog" aria-hidden="true"></span>
+        </a>
+        <a href="index.html" onClick={this.handleClick} data-toggle="tooltip" data-placement="top" title="FullScreen">
+          <span className="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+        </a>
+        <a href="index.html" onClick={this.handleClick} data-toggle="tooltip" data-placement="top" title="Lock">
+          <span className="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+        </a>
+        <a href="index.html" onClick={this.handleClick} data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+          <span className="glyphicon glyphicon-off" aria-hidden="true"></span>
+        </a>
+      </div>
+    );
+  }
 }
 
 export default SidebarFooter;
